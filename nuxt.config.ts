e442a47b0b098ad0f2c35726e0a8441a2a46db4d
ymow <kenshin0812@hotmail.com>
2023-04-20 18:06:48 +0800

    init docker nuxt3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
-import { defineNuxtConfig } from 'nuxt'
+import {defineNuxtConfig} from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    server: {
+        port: 8000, // default: 3000
+        host: '0.0.0.0' // default: localhost
+    }
+    // other configs
 })
